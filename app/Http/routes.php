1ed6f5f9d826e03c9cc0c68872a23d16745df370
Alejandro Sánchez <davidcort@users.noreply.github.com>
2017-01-14 16:58:47 -0600

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,16 @@
 |
 */
 
-Route::resource('vehiculos','VehiculoController', ['only' => ['index', 'show']]); //Solo necesitamos index y show
-Route::resource('fabricantes','FabricanteController');
-Route::resource('fabricantes.vehiculos','FabricanteVehiculoController',['except' => 'show']); //recurso anidado
+Route::group(array('prefix'=>'api/v1.0'), function(){
+    Route::resource('vehiculos','VehiculoController', ['only' => ['index', 'show']]); //Solo necesitamos index y show
+    Route::resource('fabricantes','FabricanteController');
+    Route::resource('fabricantes.vehiculos','FabricanteVehiculoController',['except' => 'show']); //recurso anidado
 //Route::get('/','VehiculoController@showAll'); //Muestra todos los vehiculos
+});
+
+
+Route::pattern('inexistente', '.*');
+Route::any('/{inexistente}', function()
+{
+	return response()->json(['mensaje' => 'Ruta o metodos incorrectos.', 'codigo' => 400],400);
+});
